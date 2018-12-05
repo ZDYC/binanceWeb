db674fec2bb9fd82e3ea618a5e29b2be8a7468a7
ZDYC <332976499@qq.com>
2018-12-05 11:26:08 +0800

    del hk_yu

--- hk_yu/trader/core/exceptions.py
+++
@@ -1,8 +0,0 @@
-class TraderBusinessException(Exception):
-    def __init__(self, code, message):
-        self.code = code
-        self.message = message
-
-
-class ExchangeException(TraderBusinessException):
-    pass
